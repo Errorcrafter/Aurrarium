655b3039d3097f7dc0e96bd72ad903660e428e5b
Errorcrafter <69189354+Errorcrafter@users.noreply.github.com>
2021-04-11 22:03:10 +0800

    made the splashscreen lead to the gui in the hackiest way possible

--- startup.py
+++ startup.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as sg
 import random
+import sys
 
 sg.theme("Black")  # colour scheme!
 
@@ -7,3 +8,6 @@ window_timeout = random.randint(800,2000)
 
 # Display splash screen
 sg.Window("areijk",no_titlebar=True,keep_on_top=True,layout=[[sg.Image(r"assets/aurarrium_logo_small.png")]]).Read(timeout=2000, close=True)
+
+# Display GUI from gui.py in the hackiest way possible
+exec(open(r"./scripts/gui.py","r",encoding="utf8").read())
