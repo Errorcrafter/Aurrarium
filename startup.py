27274bdf6d1256ba716c8b2aa2b50416679542f6
Errorcrafter <69189354+Errorcrafter@users.noreply.github.com>
2021-04-13 11:48:54 +0800

    ifjkhgc

--- startup.py
+++ startup.py
@@ -7,7 +7,7 @@ sg.theme("Black")  # colour scheme!
 window_timeout = random.randint(800,2000)
 
 # Display splash screen
-sg.Window("areijk",no_titlebar=True,keep_on_top=True,layout=[[sg.Image(r"assets/aurarrium_logo_small.png")]]).Read(timeout=2000, close=True)
+sg.Window("areijk",no_titlebar=True,keep_on_top=True,layout=[[sg.Image(r"assets/aurrarium_logo_small.png")]]).Read(timeout=2000, close=True)
 
 # Display GUI from gui.py in the hackiest way possible
 exec(open(r"./scripts/gui.py","r",encoding="utf8").read())
