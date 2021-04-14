82d2df8da91c57747bfb8d02597fd4b62ea7fbaa
Errorcrafter <69189354+Errorcrafter@users.noreply.github.com>
2021-04-14 08:09:45 +0800

    had to wreck my file strucure becuase i dum

--- scripts/aurrarium_spammer.py
+++ scripts/aurrarium_spammer.py
@@ -7,7 +7,7 @@ import prawcore
 # if __name__ == "__main__":
 #     test()
 
-def start_spam(event,values,window,reddit):
+def start_spam(event,values,window,reddit : praw.Reddit):
     print = lambda *args, **kwargs: window['-spammerOutput-'].print(*args, **kwargs)
     print("Starting Aurrarium now!")
 
@@ -21,6 +21,11 @@ def start_spam(event,values,window,reddit):
     print(f"Logged in as u/{me}")
     if (me.link_karma + me.comment_karma) < 100:
         print("Warning: Your karma level is below 100. As such, you account may not pass r/sigmaclient's karma threshold.",text_color='red')
+
+    # looping thru all posts:
+    target = reddit.subreddit("test")  # for testing purposes, will change to sigmaclient later
+    for s in target.stream.submissions():
+        pass  # WILL ADD SPAMBOT REPLY HERE
 
 if __name__ == "__main__":
     start_spam(None,None,None,None)
