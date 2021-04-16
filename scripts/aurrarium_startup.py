b1f5426e9b648ea135c2eb4ad95ac50c557661f5
Errorcrafter <69189354+Errorcrafter@users.noreply.github.com>
2021-04-16 13:41:58 +0800

    un-scuffed the startup screen

--- scripts/aurrarium_startup.py
+++ scripts/aurrarium_startup.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as sg
 import random
 import sys
+from aurrarium_gui import draw_gui
 
 sg.theme("Black")  # colour scheme!
 
@@ -9,5 +10,7 @@ window_timeout = random.randint(800,2000)
 # Display splash screen
 sg.Window("areijk",no_titlebar=True,keep_on_top=True,layout=[[sg.Image(r"assets/aurrarium_logo_small.png")]]).Read(timeout=2000, close=True)
 
-# Display GUI from aurrarium_gui.py in the hackiest way possible
-exec(open(r"scripts/aurrarium_gui.py","r",encoding="utf8").read())
+# # Display GUI from aurrarium_gui.py in the hackiest way possible
+# exec(open(r"scripts/aurrarium_gui.py","r",encoding="utf8").read())
+
+draw_gui()
