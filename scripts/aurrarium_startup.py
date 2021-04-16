94f9c89c0a1eb9178bdec8478a376def7dc0e279
Errorcrafter <69189354+Errorcrafter@users.noreply.github.com>
2021-04-16 13:42:50 +0800

    .

--- scripts/aurrarium_startup.py
+++ scripts/aurrarium_startup.py
@@ -10,7 +10,5 @@ window_timeout = random.randint(800,2000)
 # Display splash screen
 sg.Window("areijk",no_titlebar=True,keep_on_top=True,layout=[[sg.Image(r"assets/aurrarium_logo_small.png")]]).Read(timeout=2000, close=True)
 
-# # Display GUI from aurrarium_gui.py in the hackiest way possible
-# exec(open(r"scripts/aurrarium_gui.py","r",encoding="utf8").read())
-
+# Somewhat less hackier way to run aurrarium_gui.py, but there may still be issues.
 draw_gui()
